8f47b061442d43baf2b839a7d6822d61860fc009
Victor Barros Melo <victor.melo@pareva.de>
2025-03-23 20:50:21 +0000

    feat(Registry Table): Updated email to be a unique column

--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/Model/NinjaModel.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/Model/NinjaModel.java
@@ -1,6 +1,7 @@
 package dev.java10x.cadastrodeninjas.Ninjas.Model;
 
 import dev.java10x.cadastrodeninjas.Missions.Model.MissionsModel;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,56 +9,29 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_registry")
+@Data //Creates Getters and Setters Automatically
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   private String name;
+
+  @Column(unique = true)
   private String email;
+
   private int age;
+  
   @ManyToOne // A Ninja can only have one mission
   @JoinColumn(name = "missions_id") // Foreign Key from tb_missions
   private MissionsModel missions;
-
-  public NinjaModel() {
-  }
-
-  public NinjaModel(String name, String email, int age) {
-    this.name = name;
-    this.email = email;
-    this.age = age;
-  }
-
-  public Long getId() { return id; }
-
-  public void setId(Long id) { this.id = id; }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
 }
